3fa1d8745fc5520f702a419637c500f09dd3f960
Elly Chan <elly.chan.ec@gmail.com>
2019-06-29 18:14:19 +0800

    calc average length of reviews

--- read.py
+++ read.py
@@ -6,8 +6,11 @@ with open('reviews.txt', 'r') as f:
 		count += 1
 		if count % 1000 == 0:
 			print(len(data))
-print(len(data))
+print('The file has been completely accessed, there is totally', len(data), 'information in the file')
 
-print(data[0])
-print('--------------')
-print(data[1])
+sum_len = 0
+for d in data:
+	sum_len += len(d)
+	print(sum_len)
+
+print('The average length of information is', sum_len/len(data))
